f1f9cad8279989ba1177f0d3291b01ed78462569
Carlos <carlos.delgado@hotmail.it>
2020-03-24 19:21:24 +0100

    deleting Cypress examples, creating first test

---
+++ cypress/integration/JournalistCanCreateArticle.feature.js
@@ -0,0 +1,20 @@
+describe('journalist successfully creates article', () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/articles",
+      response: "fixture:success_message.json"
+    });
+    cy.visit("/");
+  });
+  it('succefully creates first article', () => {
+      cy.get("#new-article-form").within(() =>{
+        cy.get("#title-field").type("this is a title")
+        cy.get("#snippet-field").type("this is a snippet")
+        cy.get("#title-content").type("this is a content")
+        cy.get("#create-article").click()
+      })
+    cy.get("#message").should("contain", "Your article was saved")
+  })
+})
