b1000f0f820376d45ad6f656cafa0b705e7a09bb
Philip Gaunitz <fluffen@pgaunitz.se>
2020-03-25 18:26:13 +0100

    ads sad path test

--- cypress/integration/JournalistCanCreateArticle.feature.js
+++ cypress/integration/JournalistCanCreateArticle.feature.js
@@ -1,13 +1,14 @@
-describe("journalist successfully creates article", () => {
+describe("journalist can creates article", () => {
   before(() => {
     cy.server();
     cy.route({
       method: "POST",
       url: "http://localhost:3000/api/v1/articles",
-      response: "fixture:success_message.json"
+      response: "fixture:article_success_message.json"
     });
     cy.visit("/");
   });
+
   it("succefully creates first article", () => {
     cy.get("#new-article-form").within(() => {
       cy.get("#title-field").type("this is a title");
@@ -16,5 +17,26 @@ describe("journalist successfully creates article", () => {
       cy.get("#create-article").click();
     });
     cy.get("#message").should("contain", "Your article was saved");
+  });
+});
+
+describe("journalist can not create emty article", () => {
+  before(() => {
+    cy.server();
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/articles",
+      response: "fixture:article_error_message.json"
+    });
+    cy.visit("/");
+  });
+
+  it("can not create article without title", () => {
+    cy.get("#new-article-form").within(() => {
+      cy.get("#snippet-field").type("this is a snippet");
+      cy.get("#title-content").type("this is a content");
+      cy.get("#create-article").click();
+    });
+    cy.get("#message").should("contain", "Title can't be blank");
   });
 });
