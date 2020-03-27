05bd97ae741cd6516869c3aa03ad63535c148956
Janko <mailodjanka@gmail.com>
2020-03-27 14:13:03 +0100

    Refactores the article creation tests with authentication before

--- cypress/integration/JournalistCanCreateArticle.feature.js
+++ cypress/integration/JournalistCanCreateArticle.feature.js
@@ -1,12 +1,6 @@
-
 describe("journalist can creates article", () => {
   beforeEach(() => {
     cy.server();
-    cy.route({
-      method: "POST",
-      url: "/articles",
-      response: "fixture:article_success_message.json"
-    });
     cy.route({
       method: "POST",
       url: "http://localhost:3000/api/v1/auth/**",
@@ -17,11 +11,22 @@ describe("journalist can creates article", () => {
       url: "http://localhost:3000/api/v1/auth/**",
       response: "fixture:login.json"
     });
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/articles",
+      response: "fixture:article_success_message.json"
+    });
     cy.visit("/");
-
   });
 
   it("succefully creates first article", () => {
+    cy.get("#login-form").within(() => {
+      cy.get("#email").type("admin@times.ma");
+      cy.get("#password").type("password");
+      cy.get("button")
+        .contains("Login")
+        .click();
+    });
     cy.get("#new-article-form").within(() => {
       cy.get("#title-field").type("this is a title");
       cy.get("#snippet-field").type("this is a snippet");
@@ -29,28 +34,45 @@ describe("journalist can creates article", () => {
       cy.get("#category-menu").select("Tech");
       cy.get("#create-article").click();
     });
-    cy.get("#message").should("contain", "Your article was saved");
+    cy.get("#response-message").should("contain", "Your article was saved");
   });
 });
 
 describe("journalist can not create emty article", () => {
-  before(() => {
+  beforeEach(() => {
     cy.server();
     cy.route({
       method: "POST",
-      url: "https://newsroom3api.herokuapp.com/api/v1/articles",
+      url: "http://localhost:3000/api/v1/auth/**",
+      response: "fixture:login.json"
+    });
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/auth/**",
+      response: "fixture:login.json"
+    });
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/articles",
       response: "fixture:article_error_message.json"
     });
     cy.visit("/");
   });
 
   it("can not create article without title", () => {
+    cy.get("#login-form").within(() => {
+      cy.get("#email").type("admin@times.ma");
+      cy.get("#password").type("password");
+      cy.get("button")
+        .contains("Login")
+        .click();
+    });
     cy.get("#new-article-form").within(() => {
       cy.get("#snippet-field").type("this is a snippet");
       cy.get("#title-content").type("this is a content");
       cy.get("#category-menu").select("Tech");
       cy.get("#create-article").click();
     });
-    cy.get("#message").should("contain", "Title can't be blank");
+    cy.get("#response-message").should("contain", "Title can't be blank");
   });
 });
